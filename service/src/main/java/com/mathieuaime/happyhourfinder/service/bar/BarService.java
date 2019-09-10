760efd23e1188950d10d841ad4ccfabe3f43653b
Mathieu <aime.mathieu1@gmail.com>
2019-09-10 22:55:31 +0200

    #29 WIP add findByDistance method and integration tests

--- service/src/main/java/com/mathieuaime/happyhourfinder/service/bar/BarService.java
+++ service/src/main/java/com/mathieuaime/happyhourfinder/service/bar/BarService.java
@@ -2,6 +2,7 @@ package com.mathieuaime.happyhourfinder.service.bar;
 
 import com.mathieuaime.happyhourfinder.model.bar.Bar;
 import java.util.Optional;
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -25,6 +26,16 @@ public interface BarService {
    * @return the optional bar
    */
   Optional<Bar> findById(Long id);
+
+  /**
+   * Finds pageable bars by distance.
+   *
+   * @param pageable the pageable
+   * @param center the center
+   * @param distance the distance
+   * @return the pageable bars
+   */
+  Page<Bar> findByDistance(Pageable pageable, Point center, long distance);
 
   /**
    * Saves a bar.
--- service/src/main/java/com/mathieuaime/happyhourfinder/service/bar/impl/BarServiceImpl.java
+++ service/src/main/java/com/mathieuaime/happyhourfinder/service/bar/impl/BarServiceImpl.java
@@ -4,6 +4,7 @@ import com.mathieuaime.happyhourfinder.model.bar.Bar;
 import com.mathieuaime.happyhourfinder.repository.bar.BarDao;
 import com.mathieuaime.happyhourfinder.service.bar.BarService;
 import java.util.Optional;
+import org.locationtech.jts.geom.Point;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -27,6 +28,11 @@ public class BarServiceImpl implements BarService {
   @Override
   public Optional<Bar> findById(Long id) {
     return barDao.findById(id);
+  }
+
+  @Override
+  public Page<Bar> findByDistance(Pageable pageable, Point center, long distance) {
+    return barDao.findAll(pageable);
   }
 
   @Override
